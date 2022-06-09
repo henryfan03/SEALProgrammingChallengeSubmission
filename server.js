6218a6545f36b49001d63abb2806ae7966a21b8c
Henry Fan <henry.fan2313@gmail.com>
2022-06-09 15:13:12 -0400

    committed server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+
+const app = express();
+
+app.use(fileUpload());
+
+// Upload Endpoint
+app.post('/upload', (req, res) => {
+  if (req.files == null) {
+    return res.status(400).json({ msg: 'No files were uploaded'});
+  }
+
+  const file = req.files.file;
+
+  file.mv(`${__dirname}/client/public/uploads/${file.name}}`, err => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ msg: "Failure uploading"});
+    }
+
+    res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+  });
+});
+
+app.listen(5000, () => console.log('listening on port 5000'));
