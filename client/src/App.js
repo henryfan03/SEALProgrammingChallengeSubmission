f659dcf973ce3d6d3d82515b4eb2a1732f550388
Henry Fan <henry.fan2313@gmail.com>
2022-06-15 23:03:24 -0400

    replaced react favicon logo

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 const App = () => (
   <div className='container mt-4'>
     <h4 className='display-4 text-center mb-4'>
-      <i className='fab fa-react' /> SEAL Programming Challenge
+      &#128193; SEAL Programming Challenge
     </h4>
   </div>
 );
