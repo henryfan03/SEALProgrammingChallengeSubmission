ece11c822a72b8dbf1845eca1f9977895cbc617b
Henry Fan <henry.fan2313@gmail.com>
2022-06-18 01:19:09 -0400

    Removed some files and connected file upload with Axios

--- client/src/FileUpload.js
+++ client/src/FileUpload.js
@@ -1,19 +1,39 @@
 import React, { Fragment, useState} from 'react';
+import axios from 'axios';
 
 const FileUpload = () => {
   const [file, setFile] = useState('');
   const [filename, setFilename] = useState('Select a file');
+  const [uploadedFile, setUploadedFile] = useState({});
 
   const onChange = e => {
     setFile(e.target.files[0]);
     setFilename(e.target.files[0].name);
   };
 
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
     const formData = new FormData();
-    formData.append("file");
-  }
+    formData.append("file", file);
+
+    try {
+      const res = await axios.post('/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+
+      const { fileName, filePath } = res.data;
+
+      setUploadedFile( { fileName, filePath });
+    } catch (err) {
+      if(err.response.status === 500) {
+        console.log("There was a problem with the server");
+      } else {
+        console.log(err.response.data.msg);
+      }
+    }
+  };
 
   return (
     <Fragment>
