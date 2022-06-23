a2457a46cb5114ab066461b9c3225f22199249c4
Henry Fan <henry.fan2313@gmail.com>
2022-06-22 23:40:48 -0400

    Started FileDisplay component

---
+++ client/src/components/FileDisplay.js
@@ -0,0 +1,9 @@
+import React, { Fragment, useState } from 'react';
+import FileUpload from './FileUpload';
+import axios from 'axios';
+
+const FileDisplay = () {
+
+}
+
+export default FileDisplay;
