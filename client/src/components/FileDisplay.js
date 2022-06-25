c67a79df276d9efa65f99fa2d86cdc04fc7b5a8c
Henry Fan <henry.fan2313@gmail.com>
2022-06-25 01:11:06 -0400

    Created file parsing function

--- client/src/components/FileDisplay.js
+++ client/src/components/FileDisplay.js
@@ -3,8 +3,13 @@ import FileUpload from './FileUpload';
 import fs from 'fs';
 import axios from 'axios';
 
-const FileDisplay = () {
-
+const FileDisplay = () => {
+  const dir = fs.opendirSync();
+  let dirent;
+  while ((dirent = dir.readSync()) !== null) {
+    console.log(dirent.name);
+  }
+  dir.closeSync();
 };
 
 export default FileDisplay;
