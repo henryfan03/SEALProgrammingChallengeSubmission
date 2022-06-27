5f8eb35e5780101cdf49878dbb05f4783a9450c0
Henry Fan <henry.fan2313@gmail.com>
2022-06-26 22:15:22 -0400

    Installed react-router-dom

---
+++ client/src/components/Gallery.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FileUpload from './components/FileUpload';
+import FileDisplay from './components/FileDisplay';
+import './App.css';
+
+const Gallery = () => (
+  <div className='container mt-4'>
+    <h4 className='display-4 text-center mb-4'>
+      File Upload Gallery
+    </h4>
+  </div>
+);
+
+export default Gallery;
