7490ca5f7202a65b8c37d79fd7f6de96f3465b8a
Henry Fan <henry.fan2313@gmail.com>
2022-07-03 23:50:47 -0400

    added new packages

--- client/src/components/FileDisplay.js
+++ client/src/components/FileDisplay.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import FileUpload from './FileUpload';
+import Carousel from 'react-responsive-carousel';
 import { motion } from 'framer-motion';
 import fs from 'fs';
 import axios from 'axios';
