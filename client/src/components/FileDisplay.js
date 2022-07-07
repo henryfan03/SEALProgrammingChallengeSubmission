7b55dd3cceda999a488815c82e29ce703c197149
Henry Fan <henry.fan2313@gmail.com>
2022-07-07 00:01:00 -0400

    completed image rendering test

--- client/src/components/FileDisplay.js
+++ client/src/components/FileDisplay.js
@@ -8,18 +8,18 @@ const FileDisplay = () => {
   return (
     <Carousel cols={2} rows={1} gap={10} loop>
       <Carousel.Item>
-        <img width="100%" src="./client/public/thumbnails/icontxt.png" />
+        <img width="100%" src="../../thumbnails/logopdf.png" />
       </Carousel.Item>
       <Carousel.Item>
-        <img width="100%" src="https://picsum.photos/800/600?random=2" />
+        <img width="100%" src="../../thumbnails/icontxt.png" />
       </Carousel.Item>
-      <Carousel.Item>
-        <img width="100%" src="https://picsum.photos/800/600?random=3" />
-      </Carousel.Item>
-      <Carousel.Item>
-        {/* anything you want to show in the grid */}
-      </Carousel.Item>
-      {/* ... */}
+      // <Carousel.Item>
+      //   <img width="100%" src="https://picsum.photos/800/600?random=3" />
+      // </Carousel.Item>
+      // <Carousel.Item>
+      //   {/* anything you want to show in the grid */}
+      // </Carousel.Item>
+      // {/* ... */}
     </Carousel>
   )
 };
