8674e5920d5787fba15f9ab41b121c232cadd629
Henry Fan <henry.fan2313@gmail.com>
2022-07-07 23:29:06 -0400

    modified filedisplay

--- client/src/components/FileDisplay.js
+++ client/src/components/FileDisplay.js
@@ -6,20 +6,19 @@ import axios from 'axios';
 
 const FileDisplay = () => {
   return (
-    <Carousel cols={2} rows={1} gap={10} loop>
+    <Carousel cols="3" rows="1" gap="10" loop>
       <Carousel.Item>
         <img width="100%" src="../../thumbnails/logopdf.png" />
       </Carousel.Item>
       <Carousel.Item>
         <img width="100%" src="../../thumbnails/icontxt.png" />
       </Carousel.Item>
-      // <Carousel.Item>
-      //   <img width="100%" src="https://picsum.photos/800/600?random=3" />
-      // </Carousel.Item>
-      // <Carousel.Item>
-      //   {/* anything you want to show in the grid */}
-      // </Carousel.Item>
-      // {/* ... */}
+      <Carousel.Item>
+        <img width="100%" src="https://picsum.photos/800/600?random=3" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img width="100%" src="../../uploads/exampleimage.png" />
+      </Carousel.Item>
     </Carousel>
   )
 };
